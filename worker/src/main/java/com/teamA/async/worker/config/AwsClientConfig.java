ae009bb5d5d4954091c97e6cce64225163c558b2
begopa <gpdbs090@gmail.com>
2026-01-03 22:32:44 +0900

    feat:SQS Worker 메시지 파싱 및 상태 전이 처리 구현
    related to #10

--- worker/src/main/java/com/teamA/async/worker/config/AwsClientConfig.java
+++ worker/src/main/java/com/teamA/async/worker/config/AwsClientConfig.java
@@ -4,10 +4,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Configuration
 public class AwsClientConfig {
+
+    @Bean
+    public SqsClient sqsClient() {
+        return SqsClient.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .build();
+    }
 
     @Bean
     public DynamoDbClient dynamoDbClient(@Value("${aws.region}") String region) {
